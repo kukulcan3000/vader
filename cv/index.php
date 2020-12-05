0425802dad9178bd6a7ce072baeda93d65e792f9
Gabriel Tonathiu <gabriello99.cancun@gmail.com>
2020-12-04 19:13:02 -0600

    codigoweb4

---
+++ cv/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>How to put PHP in HTML - Simple Example</title>
+</head>
+<body>
+<h1><?php echo "This message is from server side." ?></h1>
+<br>
+<h2><?php echo 34 + 29 ?></h2>
+<br>
+<h3><?php echo "This is a test server side with PHP!"?></h3>
+</body>
+</html>
